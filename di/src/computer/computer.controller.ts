a839fc45b2d0a225a79f9a855b5639b1f2e82cdc
Kenny Stephens <kennethcstephens@gmail.com>
2021-08-31 12:11:05 -0700

    Organizing Code with Modules

---
+++ di/src/computer/computer.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
+
+@Controller('computer')
+export class ComputerController {
+  cpuService: CpuService;
+  diskService: DiskService;
+
+  constructor(cpuService: CpuService, diskService: DiskService) {
+    this.cpuService = cpuService;
+    this.diskService = diskService;
+  }
+
+  @Get()
+  run() {
+    return [this.cpuService.compute(1, 2), this.diskService.getData()];
+  }
+}
---
+++ di/src/cpu/cpu.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
+
+@Injectable()
+export class CpuService {
+  powerService: PowerService;
+
+  constructor(powerService: PowerService) {
+    this.powerService = powerService;
+  }
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from Power Service.');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
+}
